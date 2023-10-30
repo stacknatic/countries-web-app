f56f3dfdddc56c33ec2475e894fe27ff9d29b1bb
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-10-30 09:08:24 +0200

    restyle homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import countries from "../countries.jpeg"
 
 const Home = () => {
   return (
-    <div className='text-center mt-5'>
+    <div className='text-center mt-5 homepage-container'>
       <div>
         <span>Countries app </span>is a simple React application made in
         Business College Helsinki lessons. App uses{' '}
         <a href="https://restcountries.com/">https://restcountries.com/ </a> and{' '}
         <a href="https://openweathermap.org/">https://openweathermap.org/</a>.
-        <img src={countries} alt="Countries" className='countries-photo' />
       </div>
+        <img src={countries} alt="Countries" className='countries-photo' />
     </div>
   );
 };
